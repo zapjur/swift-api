ca844b63917f200788a4aeee3cb66ab021b88a5b
Piotr Zapiór <piotrzapior0@gmail.com>
2025-04-22 21:42:35 +0200

    Fixed: json response structure in get swift codes endpoint

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -11,6 +11,33 @@ type Handler struct {
 	repo repository.Repository
 }
 
+type BranchResponse struct {
+	Address       string `json:"address"`
+	BankName      string `json:"bankName"`
+	CountryISO2   string `json:"countryISO2"`
+	CountryName   string `json:"countryName"`
+	IsHeadquarter bool   `json:"isHeadquarter"`
+	SwiftCode     string `json:"swiftCode"`
+}
+
+type HeadquarterResponse struct {
+	Address       string               `json:"address"`
+	BankName      string               `json:"bankName"`
+	CountryISO2   string               `json:"countryISO2"`
+	CountryName   string               `json:"countryName"`
+	IsHeadquarter bool                 `json:"isHeadquarter"`
+	SwiftCode     string               `json:"swiftCode"`
+	Branches      []BranchInHQResponse `json:"branches"`
+}
+
+type BranchInHQResponse struct {
+	Address       string `json:"address"`
+	BankName      string `json:"bankName"`
+	CountryISO2   string `json:"countryISO2"`
+	IsHeadquarter bool   `json:"isHeadquarter"`
+	SwiftCode     string `json:"swiftCode"`
+}
+
 func NewHandler(repo repository.Repository) *Handler {
 	return &Handler{repo: repo}
 }
@@ -29,20 +56,52 @@ func (h *Handler) GetSwiftCode(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
+
 	if code.IsHeadquarter {
 		branches, err := h.repo.GetBranchesByHeadquarter(swiftCode)
 		if err != nil {
 			http.Error(w, "Error retrieving branches", http.StatusInternalServerError)
 			return
 		}
-		code.Branches = branches
+		var branchResponses []BranchInHQResponse
+		for _, b := range branches {
+			branchResponses = append(branchResponses, BranchInHQResponse{
+				Address:       *b.Address,
+				BankName:      b.BankName,
+				CountryISO2:   b.CountryISO2,
+				IsHeadquarter: b.IsHeadquarter,
+				SwiftCode:     b.SwiftCode,
+			})
+		}
+
+		resp := HeadquarterResponse{
+			Address:       *code.Address,
+			BankName:      code.BankName,
+			CountryISO2:   code.CountryISO2,
+			CountryName:   code.CountryName,
+			IsHeadquarter: true,
+			SwiftCode:     code.SwiftCode,
+			Branches:      branchResponses,
+		}
+
+		if err = json.NewEncoder(w).Encode(resp); err != nil {
+			http.Error(w, "Error encoding response", http.StatusInternalServerError)
+		}
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	resp := BranchResponse{
+		Address:       *code.Address,
+		BankName:      code.BankName,
+		CountryISO2:   code.CountryISO2,
+		CountryName:   code.CountryName,
+		IsHeadquarter: false,
+		SwiftCode:     code.SwiftCode,
+	}
 
-	if err := json.NewEncoder(w).Encode(code); err != nil {
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
 		http.Error(w, "Error encoding response", http.StatusInternalServerError)
-		return
 	}
 }
 
